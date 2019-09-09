b04bd427c81e59a7c8d73ea4695bfb56a284d070
Samplasion <samplasion@gmail.com>
2019-09-09 17:58:50 +0200

    message

--- publi.sh
+++ publi.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
+
+message="[Auto-Publish] New tweak"
+
+while [ "$1" != "" ]; do
+    if [ "$1" == "-m" ]; then
+        shift
+        message=$1
+    fi
+    shift
+done
+
 cp ./Packages ./Packages_cp
 rm -f ./Packages.bz2
 bzip2 Packages
 mv ./Packages_cp ./Packages
 git add .
-git commit -m "[Auto-Publish] New tweak"
+git commit -m message
 git push
